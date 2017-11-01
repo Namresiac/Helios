26f3a3c2e4228983639f4630be2e3eea94dd2b08
Namresiac <nicolas.caiserman@gmail.com>
2017-11-01 18:11:30 +0200

    second

--- ex05/bc.sh
+++ ex05/bc.sh
@@ -1,24 +1,80 @@
 #!/bin/bash
 
+if [[ $1 == "-s" || $1 == "-e" || $1 == "-o" || $1 == "-m" ]]
+then
 case "$1" in
-  -s)
-    #faire la somme de tous les nombres
-;;
+-s) #faire la somme de tous les nombres
+  if [[ $2 && $2 == ?(-)+([0-9]) ]]
+  then
+    sum=0
+    for i in $@
+    do
+      sum=$((sum+i))
+    done
+  echo $sum
+    exit 0
+  else
+    echo "Give some number(s)"
+    exit 1
+  fi
+  ;;
+
+-e) #faire la somme des nombres pairs
+  if [[ $2 && $2 == ?(-)+([0-9]) ]]
+  then
+    sum=0
+      for i in $@
+      do
+        if [[ "i%2" -eq "0" ]]
+        then
+          sum=$((sum+i))
+        fi
+      done
+      echo $sum
+    exit 0
+  else
+    echo "Give some number(s)"
+    exit 1
+  fi
+  ;;
+-o) # faire la somme des nombres impairs
+  if [[ $2 && $2 == ?(-)+([0-9]) ]]
+  then
+    sum=0
+      for i in $@
+      do
+        if [[ "i%2" -ne "0" ]]
+        then
+          sum=$((sum+i))
+        fi
+      done
+      echo $sum
+    exit 0
+  else
+    echo "Give some number(s)"
+    exit 1
+  fi
+  ;;
+
+-m) #faire la moyenne de tous les nombres
+  if [[ $2 && $2 == ?(-)+([0-9]) ]]
+  then
+    sum=0
+    for i in $@
+    do
+      sum=$((sum+i))
+      tot=$(($#-1))
+    done
+    echo "$sum/$tot" | bc
+   exit 0
+  else
+   echo "Give some number(s)"
+   exit 1
+  fi
+  ;;
 esac
 
-case "$1" in
-  -e)
-    #faire la somme des nombres pairs
-;;
-esac
-
-case "$1" in
-  -o)
-    # faire la somme des nombres impairs
-;;
-esac
-
-case "$1" in
-  -m)
-    #faire la moyenne de tous les nombres
-esac
+else
+  echo "Give an argument"
+  exit 1
+fi
